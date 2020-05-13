1b71e9cc71e94b757998f64d19cb84ade3aa471e
sztuczkap <p.sztuczka@wp.pl>
2020-05-13 21:30:53 +0200

    adding bookController

---
+++ src/main/java/sztuczkap/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,25 @@
+package sztuczkap.springframework.spring5webapp.controller;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sztuczkap.springframework.spring5webapp.repositories.BookRepository;
+
+@Component
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
